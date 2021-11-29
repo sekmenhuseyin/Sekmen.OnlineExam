bb70f1084d1db628b9aa5152d8d2c508a4406bb5
Hüseyin Sekmenoğlu <huseyin@sekmen.dev>
2021-11-29 16:29:44 +0300

    answer service added

---
+++ src/Sekmen.OnlineExam.Application/ExamsQuestionsAnswers/Dto/AnswerDto.cs
@@ -0,0 +1,20 @@
+﻿using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+using Sekmen.OnlineExam.Exams;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sekmen.OnlineExam.ExamsQuestionsAnswers.Dto
+{
+    [AutoMapFrom(typeof(Answer))]
+    public class AnswerDto : AuditedEntityDto<Guid>
+    {
+        [Required]
+        public string Name { get; set; }
+
+        public bool IsCorrect { get; set; }
+
+        [Required]
+        public int Order { get; set; }
+    }
+}
