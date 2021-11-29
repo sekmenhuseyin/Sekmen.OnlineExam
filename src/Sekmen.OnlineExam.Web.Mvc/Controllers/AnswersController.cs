4880479469f0ac5cc226ea683d15e64ac330d4cc
Hüseyin Sekmenoğlu <huseyin@sekmen.dev>
2021-11-29 17:07:09 +0300

    frontend for answers

---
+++ src/Sekmen.OnlineExam.Web.Mvc/Controllers/AnswersController.cs
@@ -0,0 +1,30 @@
+﻿using Abp.Application.Services.Dto;
+using Microsoft.AspNetCore.Mvc;
+using Sekmen.OnlineExam.Controllers;
+using Sekmen.OnlineExam.ExamsQuestionsAnswers;
+using System;
+
+namespace Sekmen.OnlineExam.Web.Controllers
+{
+    public class AnswersController : OnlineExamControllerBase
+    {
+        private readonly IAnswerAppService _answerAppService;
+
+        public AnswersController(IAnswerAppService answerAppService)
+        {
+            _answerAppService = answerAppService;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult EditModal(Guid questionId)
+        {
+            var question = _answerAppService.Get(new EntityDto<Guid>(questionId));
+            return PartialView("_EditModal", question);
+        }
+    }
+}
